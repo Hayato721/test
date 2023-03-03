9a9220491945af72ebec8ae560c9c431d5eb38f9
Hayato721 <g031s111@s.iwate-pu.ac.jp>
2023-03-03 13:59:14 +0900

    インデントを調整

--- src/javaapplication3/kadai3.java
+++ src/javaapplication3/kadai3.java
@@ -9,18 +9,19 @@ package javaapplication3;
  * @author hayat
  */
 public class kadai3 {
-    public static void main (String[] args) {
-        int i;
-        for(i = 1; i <= 100; i++) {
-            if(i % 15 == 0) {
-            System.out.println("fizzbuzz");
-            }else if(i % 3 == 0) {
-            System.out.println("fizz");
-            }else if(i % 5 == 0) {
-            System.out.println("buzz");
-            }else {
-            System.out.println(i);
-            }
-        }
+
+  public static void main(String[] args) {
+    int i;
+    for (i = 1; i <= 100; i++) {
+      if (i % 15 == 0) {
+        System.out.println("fizzbuzz");
+      } else if (i % 3 == 0) {
+        System.out.println("fizz");
+      } else if (i % 5 == 0) {
+        System.out.println("buzz");
+      } else {
+        System.out.println(i);
+      }
     }
+  }
 }
