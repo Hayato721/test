0d1e6ec325976231596197a8b266d9fc08fbc675
Hayato721 <g031s111@s.iwate-pu.ac.jp>
2023-03-03 14:19:02 +0900

    課題3-2の完成

---
+++ src/javaapplication3/kadai3_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author hayat
+ */
+public class kadai3_2 {
+
+  public static void main(String[] args) {
+    int i = 1;
+    while (i <= 100) {
+      if (i % 15 == 0) {
+        System.out.println("fizzbuzz");
+      } else if (i % 3 == 0) {
+        System.out.println("fizz");
+      } else if (i % 5 == 0) {
+        System.out.println("buzz");
+      } else {
+        System.out.println(i);
+      }
+      i++;
+    }
+  }
+}
